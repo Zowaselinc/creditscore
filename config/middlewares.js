6638c82b79a70031726bf688b14afd14af2d29ce
TeamZowasel <113076845+TeamZowasel@users.noreply.github.com>
2023-03-24 01:48:45 -0700

    Add files via upload

---
+++ config/middlewares.js
@@ -0,0 +1,13 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  { name: 'strapi::body', config: { includeUnparsed: true } },
+  // 'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
